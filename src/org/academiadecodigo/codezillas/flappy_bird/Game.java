8c50219bd06363cf91e95c94f2fb825021bd0ebb
Nelson Sa <nelsonysa@gmail.com>
2019-06-20 11:18:27 +0000

    Updated .gitignore and added SimpleGraphics library

--- src/org/academiadecodigo/codezillas/flappy_bird/Game.java
+++ src/org/academiadecodigo/codezillas/flappy_bird/Game.java
@@ -1,4 +1,6 @@
 package org.academiadecodigo.codezillas.flappy_bird;
 
 public class Game {
+
+
 }
--- src/org/academiadecodigo/codezillas/flappy_bird/Main.java
+++ src/org/academiadecodigo/codezillas/flappy_bird/Main.java
@@ -3,5 +3,7 @@ package org.academiadecodigo.codezillas.flappy_bird;
 public class Main {
     public static void main(String[] args) {
 
+        
+
     }
 }
