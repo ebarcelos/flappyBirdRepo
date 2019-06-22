cf562834eb9572913738632f77d7fd9b0fb4d23b
Rodrigo Ourique <rodrigoourique@gmail.com>
2019-06-22 21:47:01 +0000

    Obstacle Factory with positions with Obstacle constructor <- (example)

--- src/org/academiadecodigo/codezillas/flappy_bird/Element/Obstacle.java
+++ src/org/academiadecodigo/codezillas/flappy_bird/Element/Obstacle.java
@@ -1,4 +1,11 @@
 package org.academiadecodigo.codezillas.flappy_bird.Element;
 
+import org.academiadecodigo.codezillas.flappy_bird.Position.Position;
+
 public class Obstacle {
+    private Position pos;
+
+    public Obstacle(Position pos){
+        this.pos = pos;
+    }
 }
--- src/org/academiadecodigo/codezillas/flappy_bird/Position/ObstacleFactory.java
+++ src/org/academiadecodigo/codezillas/flappy_bird/Position/ObstacleFactory.java
@@ -1,13 +1,16 @@
 package org.academiadecodigo.codezillas.flappy_bird.Position;
 
-
 import org.academiadecodigo.codezillas.flappy_bird.Element.Obstacle;
 
 public class ObstacleFactory {
 
-    public Obstacle[] getObstacle() {
+    public static Obstacle[] getNewObstacle () {
 
-        Obstacle[] obstacle = new Obstacle[] {};
+        Position posObstacleUp = new Position(1280, 0);
+        Position posObstacleDown = new Position(1280, 400);
+        Obstacle[] obstacle = new Obstacle[2];
+        obstacle[0] = new Obstacle(posObstacleUp);
+        obstacle[1] = new Obstacle(posObstacleDown);
         return obstacle;
     }
 }
