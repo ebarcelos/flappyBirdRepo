b6d2e74be29b26cdb8ee48d10ae1604b171c8da6
Emanuel Barcelos <ebarcelos@gmail.com>
2019-06-23 17:46:41 +0000

    Created diferent types of obstacles double, singleTop and singleBottom

--- src/org/academiadecodigo/codezillas/flappy_bird/Element/RunObstacle.java
+++ src/org/academiadecodigo/codezillas/flappy_bird/Element/RunObstacle.java
@@ -1,25 +1,47 @@
 package org.academiadecodigo.codezillas.flappy_bird.Element;
 
+import org.academiadecodigo.codezillas.flappy_bird.Position.ObstacleFactory;
+
 public class RunObstacle extends Thread {
+
+    private Obstacle[] obstacles = new Obstacle[2];
 
     @Override
     public void run() {
-        Obstacle obstacle = new Obstacle();
-        obstacle.init();
+        obstacles[0] = ObstacleFactory.getNewObstacle();
 
         try {
             while (true) {
-
                 Thread.sleep(5);
-                obstacle.moveObstacle();
-                if (obstacle.hasPassedBird()) {
-                    obstacle = new Obstacle();
-                    obstacle.init();
+
+                for ( int i=0; i<obstacles.length; i++) {
+
+                    if (obstacles[i] != null) {
+                        obstacles[i].moveObstacle();
+
+                        if (obstacles[i].hasPassedBird()) {
+                            obstacles[i] = ObstacleFactory.getNewObstacle();
+                            obstacles[i].init();
+                        }
+
+                        if (obstacles[i].hasPassedMiddle()){
+                            fillObstacles();
+                        }
+                    }
+
                 }
 
             }
         } catch (InterruptedException e) {
             System.out.println("deu merda no obstacle");
+        }
+    }
+
+    public void fillObstacles() {
+        for (int i=0; i<obstacles.length; i++) {
+            if (obstacles[i] == null) {
+                obstacles[i] = ObstacleFactory.getNewObstacle();
+            }
         }
     }
 }
---
+++ src/org/academiadecodigo/codezillas/flappy_bird/Element/SingleBottomObstacle.java
@@ -0,0 +1,53 @@
+package org.academiadecodigo.codezillas.flappy_bird.Element;
+
+import org.academiadecodigo.codezillas.flappy_bird.Position.Position;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class SingleBottomObstacle implements Obstacle {
+
+    private Position position;
+    private Rectangle bottomRectangle;
+    private Picture picture;
+    private boolean passedBird;
+    private int obstWidth = 100;
+    private int obstHeight = 460;
+
+    public SingleBottomObstacle (int width, int height, ObstacleType type) {
+        this.obstWidth = width;
+        this.obstHeight = height;
+
+        position = new Position(Position.width, 0);
+        bottomRectangle = new Rectangle(position.getX(), 0, obstWidth, obstHeight);
+
+        picture = new Picture(bottomRectangle.getX(), bottomRectangle.getY(), type.getPath());
+    }
+
+    public void init() {
+        bottomRectangle.setColor(Color.ORANGE);
+        bottomRectangle.fill();
+        picture.draw();
+    }
+
+    @Override
+    public void moveObstacle() {
+        int initX = position.getX();
+        position.setX(position.getX() - 1);
+        bottomRectangle.translate(position.getX() - initX, 0);
+        checkFinalPosition();
+    }
+
+    private void checkFinalPosition () {
+        this.passedBird = (this.position.getX() + obstWidth) < 0;
+    }
+
+    public boolean hasPassedBird () {
+        return passedBird;
+    }
+
+    public boolean hasPassedMiddle() {
+        return position.getX() < (position.getWidth()/2);
+    }
+
+}
