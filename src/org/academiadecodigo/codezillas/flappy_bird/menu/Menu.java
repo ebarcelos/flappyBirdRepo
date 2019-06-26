172ff7446a70e1dea8eb83295804362d98ddd897
Nelson Sa <nelsonysa@gmail.com>
2019-06-26 18:13:41 +0000

    Added mouse event

--- src/org/academiadecodigo/codezillas/flappy_bird/menu/Menu.java
+++ src/org/academiadecodigo/codezillas/flappy_bird/menu/Menu.java
@@ -4,19 +4,26 @@ import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseEventType;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Menu implements KeyboardHandler {
+public class Menu implements KeyboardHandler, MouseHandler {
 
     private static Picture picture;
     private static boolean hasFinished;
     private Keyboard keyboard = new Keyboard(this);
+    private Mouse mouse = new Mouse(this);
 
     public Menu() {
         picture = new Picture(0, 0, "resources/img/Background/startMenu.jpg");
     }
 
     public void initMenu() {
+
+        mouse.addEventListener(MouseEventType.MOUSE_CLICKED);
         KeyboardEvent spacePress = new KeyboardEvent();
 
         spacePress.setKey(KeyboardEvent.KEY_SPACE);
@@ -36,4 +43,17 @@ public class Menu implements KeyboardHandler {
     }
 
     public void keyReleased(KeyboardEvent var1) {}
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        System.out.println(mouseEvent.getX() + " " + mouseEvent.getY());
+            picture.delete();
+            hasFinished = true;
+        System.out.println("working");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
 }
